536bf71166429228c075163e84050357a5e8f3d8
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2024-10-09 22:13:12 +0530

    update

---
+++ js_notes_bro_code/day-3 arithmetic operators in js/index.js
@@ -0,0 +1,35 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+let students = 30;
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students ** 2;
+// students = students / 2;
+// let extrastudents = students % 2;
+
+// another way to do this is augmented assignment operators
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students **= 2;
+// students /= 2;
+// students %= 2;
+
+// increment operators
+students++;
+// decrement operators
+students--;
+/*
+operator precedence
+1. parenthesis()
+2. exponent
+3. multiplication & division & modulo
+4. addition & subtraction 
+*/
+let result = 1 + 2 / 4 - 3 * 4(5 + 2)
+console.log(students)
+console.log(extrastudents)
+console.log(result)
