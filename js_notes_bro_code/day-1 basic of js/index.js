b7c3bf7dde4c695de76569193c5366cae16417a9
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2025-08-19 17:58:28 +0530

    react-start

--- js_notes_bro_code/day-1 basic of js/index.js
+++
@@ -1,9 +0,0 @@
-// console.log('hello gourav this is you first js code how its going');
-// console.log('its going good');
-// this is comment single-line
-/*this is also a comment multi-line*/
-
-window.alert('this is alert');
-window.alert('this is sencord alert')
-document.getElementById("myH1").textContent = 'hello';
-document.getElementById("myP").textContent = 'world of js i am here also';
